e8a99d97a67707607d61509970efdc56c924f121
Rob Cobb <rwcobbjr@gmail.com>
2018-12-23 16:56:18 -0500

    update plugin-preact with context shims

--- packages/gatsby-plugin-preact/src/gatsby-node.js
+++ packages/gatsby-plugin-preact/src/gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require('path')
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
   // Requiring the server version of React-dom is hardcoded right now
   // in the development server. So we'll just avoid loading Preact there
@@ -6,9 +7,12 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
     actions.setWebpackConfig({
       resolve: {
         alias: {
-          react: `preact-compat`,
+          // preact-compat does not yet ship with createContext as an export
+          // instead, use our file that imports everything from preact-compat and exports it back out, but this time with createContext included
+          react: path.resolve(__dirname, './preact-compat'),
           "react-dom": `preact-compat`,
           "create-react-class": `preact-compat/lib/create-react-class`,
+          "create-react-context": `preact-context/dist/context`,
         },
       },
     })
---
+++ packages/gatsby-plugin-preact/src/preact-compat.js
@@ -0,0 +1,10 @@
+import * as compat from 'preact-compat'
+import { createContext } from 'preact-context'
+export * from 'preact-compat'
+
+export { createContext }
+
+export default {
+ createContext,
+  ...compat
+}
